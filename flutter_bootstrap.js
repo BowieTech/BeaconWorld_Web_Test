0f4915618dac19c11093390839fadbe9d3d00f6c
ChloeHuang <chloehuang.zq@gmail.com>
2025-06-29 17:04:31 +1200

    图片路由更新

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "90035020"
+    serviceWorkerVersion: "3493201405"
   }
 });
